03d171069cec8560ee3af5ec95b3ebef1173d154
devrokket <rokany@cau.ac.kr>
2024-05-01 22:22:17 +0900

    test: Jenkins build test main 또는 develop에 push 했을 때 chmod +x gradlew 및 ./gradlew clean build 실행

--- src/main/java/com/taste/zip/tastezip/controller/HealthCheckController.java
+++ src/main/java/com/taste/zip/tastezip/controller/HealthCheckController.java
@@ -1,6 +1,6 @@
 package com.taste.zip.tastezip.controller;
 
-import org.springframework.http.MediaType;
+// build shell test
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
