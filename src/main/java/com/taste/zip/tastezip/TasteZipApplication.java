52bbe9e3e3b53e78b25af9656d79c0c67558e755
devrokket <rokany@cau.ac.kr>
2024-05-01 11:36:44 +0900

    chore: project setting

---
+++ src/main/java/com/taste/zip/tastezip/TasteZipApplication.java
@@ -0,0 +1,14 @@
+package com.taste.zip.tastezip;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TasteZipApplication {
+	// test
+
+	public static void main(String[] args) {
+		SpringApplication.run(TasteZipApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/taste/zip/tastezip/TasteZipApplicationTests.java
@@ -0,0 +1,13 @@
+package com.taste.zip.tastezip;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TasteZipApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
