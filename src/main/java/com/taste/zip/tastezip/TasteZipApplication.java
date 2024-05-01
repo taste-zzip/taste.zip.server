7b4c4d82d1757b8491fe345e28518c2b233e102d
devrokket <rokany@cau.ac.kr>
2024-05-02 01:35:09 +0900

    test: CI/CD test

--- src/main/java/com/taste/zip/tastezip/TasteZipApplication.java
+++ src/main/java/com/taste/zip/tastezip/TasteZipApplication.java
@@ -2,8 +2,7 @@ package com.taste.zip.tastezip;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration; // CI 과정에서 임시적으로 exclude
-// test
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration; // CI 과정에서 임시적으로 exclude, DB 설정 후 지우기
 
 @SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
 public class TasteZipApplication {
