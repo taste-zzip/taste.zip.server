4ef5425127662d180b41a7049caf0ea59c8d08ce
devrokket <rokany@cau.ac.kr>
2024-05-04 13:49:59 +0900

    chore: Datasource autoconfigure 삭제

--- src/main/java/com/taste/zip/tastezip/TasteZipApplication.java
+++ src/main/java/com/taste/zip/tastezip/TasteZipApplication.java
@@ -2,8 +2,6 @@ package com.taste.zip.tastezip;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration; // CI 과정에서 임시적으로 exclude, DB 설정 후 지우기
-
 @SpringBootApplication
 public class TasteZipApplication {
 	public static void main(String[] args) {
