828fcf6c4421bc1cb8884b8d51825023fc11b498
devrokket <rokany@cau.ac.kr>
2024-05-09 17:46:38 +0900

    chore: 백업 전 수정사항 반영

--- src/main/java/com/taste/zip/tastezip/dto/CafeteriaResponse.java
+++ src/main/java/com/taste/zip/tastezip/dto/CafeteriaResponse.java
@@ -1,10 +1,19 @@
 package com.taste.zip.tastezip.dto;
 
+import com.taste.zip.tastezip.entity.Cafeteria;
+
 public record CafeteriaResponse(
     Long cafeteriaId,
     String cafeteriaName,
     String streetAddress,
     int videoCnt // 식당에 등록된 영상 개수
     ) {
-
+    public static CafeteriaResponse from(Cafeteria cafeteria) {
+        return new CafeteriaResponse(
+            cafeteria.getId(),
+            cafeteria.getName(),
+            cafeteria.getStreetAddress(),
+            cafeteria.getVideoCnt()
+        );
+    }
 }
--- src/main/java/com/taste/zip/tastezip/service/CafeteriaService.java
+++ src/main/java/com/taste/zip/tastezip/service/CafeteriaService.java
@@ -1,5 +1,9 @@
 package com.taste.zip.tastezip.service;
 
+import com.taste.zip.tastezip.dto.CafeteriaResponse;
+import com.taste.zip.tastezip.repository.CafeteriaRepository;
+import java.util.Collections;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -8,5 +12,18 @@ import org.springframework.transaction.annotation.Transactional;
 @Transactional(readOnly = true)
 @RequiredArgsConstructor
 public class CafeteriaService {
+
+    private final CafeteriaRepository cafeteriaRepository;
+
+    public List<CafeteriaResponse> findByKeyword(String keyword) {
+        if (keyword.isBlank()) { // 검색어가 없을 경우
+            return Collections.emptyList();
+        }
+
+        return cafeteriaRepository.findByNameContainingAndTypeContaining(keyword)
+            .stream()
+            .map(CafeteriaResponse::from)
+            .toList();
+    }
 
 }
