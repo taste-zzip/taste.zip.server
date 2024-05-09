595d315089f7eb1fc85f4f93b660c7fc97ad2866
devrokket <rokany@cau.ac.kr>
2024-05-09 17:46:27 +0900

    feat: cafeteria controller 작성

--- src/main/java/com/taste/zip/tastezip/controller/CafeteriaController.java
+++ src/main/java/com/taste/zip/tastezip/controller/CafeteriaController.java
@@ -1,8 +1,13 @@
 package com.taste.zip.tastezip.controller;
 
+import com.taste.zip.tastezip.dto.CafeteriaResponse;
 import com.taste.zip.tastezip.service.CafeteriaService;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -11,5 +16,11 @@ import org.springframework.web.bind.annotation.RestController;
 public class CafeteriaController {
 
     private final CafeteriaService cafeteriaService;
+
+    @GetMapping("/cafeteria/list")
+    public ResponseEntity<List<CafeteriaResponse>> findCafeteriaByKeyword(@RequestParam(value = "keyword") String keyword) {
+        List<CafeteriaResponse> responses = cafeteriaService.findByKeyword(keyword);
+        return ResponseEntity.ok(responses);
+    }
 
 }
