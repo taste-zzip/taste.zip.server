a1be53f298cac49f5004e6ec596ead67eaaf3087
Lee Euije <exitfill1234@naver.com>
2024-05-16 21:09:34 +0900

    feat: 필요없는 코드 제거

--- src/main/java/com/taste/zip/tastezip/dto/AuthRegistrationResponse.java
+++ src/main/java/com/taste/zip/tastezip/dto/AuthRegistrationResponse.java
@@ -1,9 +1,7 @@
 package com.taste.zip.tastezip.dto;
 
 import com.taste.zip.tastezip.auth.TokenDetail;
-import jakarta.validation.Valid;
 
-@Valid
 public record AuthRegistrationResponse(
     TokenDetail tokenDetail,
     String accessToken,
