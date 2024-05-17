a05254069111c6bdfeb2dbc68d65468349daa1f4
Lee Euije <exitfill1234@naver.com>
2024-05-17 14:09:54 +0900

    fix: spring boot test 삭제

--- src/test/java/com/taste/zip/tastezip/TasteZipApplicationTests.java
+++ src/test/java/com/taste/zip/tastezip/TasteZipApplicationTests.java
@@ -1,9 +1,7 @@
 package com.taste.zip.tastezip;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class TasteZipApplicationTests {
 
 	@Test
