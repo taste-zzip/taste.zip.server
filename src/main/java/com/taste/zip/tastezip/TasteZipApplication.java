4cc78ae9ed4d0fbf3934fe0da022ff5d1ccaccac
Lee Euije <exitfill1234@naver.com>
2024-05-17 14:22:55 +0900

    fix: swagger server url 수정

--- src/main/java/com/taste/zip/tastezip/TasteZipApplication.java
+++ src/main/java/com/taste/zip/tastezip/TasteZipApplication.java
@@ -1,10 +1,16 @@
 package com.taste.zip.tastezip;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
+import java.util.Arrays;
 import java.util.TimeZone;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
+@OpenAPIDefinition(
+    servers = {@Server(url = "/")}
+)
 @SpringBootApplication
 public class TasteZipApplication {
 	public static void main(String[] args) {
