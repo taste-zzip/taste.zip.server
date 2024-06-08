d16e269be04396d5b86cba10c3615fb369c127b9
Lee Euije <exitfill1234@naver.com>
2024-06-09 02:01:40 +0900

    fix: auditing config 추가

---
+++ src/main/java/com/taste/zip/tastezip/config/PersistenceConfig.java
@@ -0,0 +1,10 @@
+package com.taste.zip.tastezip.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@Configuration
+@EnableJpaAuditing
+public class PersistenceConfig {
+
+}
--- src/main/java/com/taste/zip/tastezip/entity/AuditingEntity.java
+++ src/main/java/com/taste/zip/tastezip/entity/AuditingEntity.java
@@ -7,16 +7,18 @@ import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 @Getter
 @MappedSuperclass
+@SuperBuilder
 @AllArgsConstructor
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @EntityListeners(AuditingEntityListener.class)
-public class AuditingEntity {
+public abstract class AuditingEntity {
 
     @CreatedDate
     protected LocalDateTime createdAt;
